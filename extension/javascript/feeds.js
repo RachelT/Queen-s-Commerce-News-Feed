4f49507756b6c8edf20bf95fff641b70ef6afa6e
Draco Li <draco@dracoli.com>
2011-11-13 17:01:37 -0500

    Project starting to look great!

--- extension/javascript/feeds.js
+++ extension/javascript/feeds.js
@@ -27,22 +27,46 @@ function openNewShareWindow(id, url) {
   window.open(openUrl + newsUrl, '_blank', 'resizable=0, scrollbars=0, width=690, height=415');
 }
 
+function openNewContentWindowWithContent(content) {
+	window.open('<div><h2>I love cheese</h2></div>', '', 'toolbar=no, width=300, height=400');
+}
+
 /**
  * Fetch the feed from our website
  */
 function fetch_feed() {
-  chrome.extension.sendRequest({'action': 'fetch_feed', 'url': 'https://www.dracoli.com/commercefeeds/all.json'},
+  chrome.extension.sendRequest({'action': 'fetch_feed', 'url': 'http://www.dracoli.com/commercefeeds/server/cache/feeds.json'},
     function(response) {
-      display_feeds(feeds);
+      display_feeds(response);
     }
   );
 }
 
 function display_feeds(feeds) {
-	for (var feed in feeds) {
-		var html = '<div class="feed">\
-									<a href="' + feed.url + '">' + feed.title + '</a>\
-							  </div>';
-		$('#content #feeds').append(html);
+	feeds = $.parseJSON(feeds);
+	for (var oneKey in feeds) {
+		// Get our content for this category
+		var catFeeds = feeds[oneKey],
+				sectionHTML = '';
+		
+		// Create new section - even if empty
+		sectionHTML = '<section>\
+										<div class="sectionHeader"> \
+											<h4>' + oneKey + '<small class="stats"> \
+											' + catFeeds.length + ' Feeds</small></h4> \
+										</div> \
+										<ul class="sectionContent unstyled"> \
+										</ul> \
+									</section>';
+		$('#content #feeds').append(sectionHTML);
+		
+		// Add feeds in the section
+		for (var i = 0; i < catFeeds.length; i++) {
+			var feed = catFeeds[i],
+					html = '<li class="feed"> \
+										<a href="' + feed.url + '">' + feed.title + '</a> \
+								  </li>';
+			$('section:last .sectionContent').append(html);
+		}
 	}
 }
